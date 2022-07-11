079e2630cff3fd430a0c9e39bba9594df55160de
ArmandoBeltran <beltran.armando2210@gmail.com>
2022-07-11 13:59:31 -0500

    Ahora el precio de lista se recalcula cada vez que uno de sus parametros cambia, con el fin de hacerlo más dinámico

--- saborati/models/product_margin.py
+++ saborati/models/product_margin.py
@@ -10,11 +10,3 @@ class ProductMargin(models.Model):
 
     margin = fields.Float('Margin')
     product_tmpl_id = fields.Many2one('product.template')
-
-    @api.model
-    def create(self, vals_list): 
-        margin = super(ProductMargin, self).create(vals_list)
-
-        margin.product_tmpl_id.list_price = margin.product_tmpl_id.replacement_cost / (1 - margin.margin)
-
-        return margin 
--- saborati/models/product_template.py
+++ saborati/models/product_template.py
@@ -9,3 +9,14 @@ class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
     margin_ids = fields.One2many('product.margin', 'product_tmpl_id')
+
+    list_price = fields.Float(compute="_compute_price")
+
+
+    @api.depends('margin_ids', 'replacement_cost')
+    def _compute_price(self): 
+        for record in self:
+            record.list_price = 1.0
+            margin = self.env['product.margin'].search([('product_tmpl_id', '=', record.id)], order='create_date desc', limit=1).margin
+            if margin and record.replacement_cost:   
+                record.list_price = record.replacement_cost / (1 - margin)
