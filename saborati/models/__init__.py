c00143fe3e21513c12964d00eb1cd8e18e04f4a9
ArmandoBeltran <beltran.armando2210@gmail.com>
2022-07-19 16:11:57 -0500

    Se agrega modelo para brands y correcciones en vistas de categorias

--- saborati/models/__init__.py
+++ saborati/models/__init__.py
@@ -6,3 +6,4 @@ from . import mrp_bom
 from . import product_margin
 from . import product_template
 from . import product_category
+from . import brand
---
+++ saborati/models/brand.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, api, fields, _ 
+
+import logging 
+_logger = logging.getLogger(__name__)
+
+
+class Brand(models.Model): 
+    _name = 'brand'
+    _inherit = ['image.mixin']
+
+    name = fields.Char('Name')
