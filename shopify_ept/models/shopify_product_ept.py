a5b660c67f2e9f0b404651c098ea53c502e8417b
ArmandoBeltran <beltran.armando2210@gmail.com>
2022-10-03 13:41:11 -0500

    Se quita la funcion de prepare_shopify_collection_for_update_export en shopify.product.product.ept

--- shopify_ept/models/shopify_product_ept.py
+++ shopify_ept/models/shopify_product_ept.py
@@ -265,19 +265,11 @@ class ShopifyProductProductEpt(models.Model):
 
         for template in templates:
             new_product = shopify.Product()
-            _logger.info(new_product)
 
             self.prepare_shopify_product_for_update_export(new_product, template, instance, is_set_basic_detail,
                                                            is_publish, is_set_price)
 
             result = new_product.save()
-            
-            new_collection = shopify.CustomCollection()
-
-            self.prepare_shopify_collection_for_update_export(new_collection, template)
-
-            res = new_collection.save()
-            
 
             if not result:
                 message = "Product %s not exported in Shopify Store." % template.name
