8528393a4b71f2d496bd72a271bb2c23ae15f8bd
ArmandoBeltran <beltran.armando2210@gmail.com>
2022-10-06 17:50:19 -0500

    Soluciones

--- saborati/models/res_partner.py
+++ saborati/models/res_partner.py
@@ -4,10 +4,6 @@ from odoo import models, api, fields, _
 
 class ResPartner(models.Model): 
     _inherit = 'res.partner'
-    
-    company_type = fields.Selection(string='Company Type',
-        selection=[('person', 'Individual'), ('company', 'Company')],
-        compute='_compute_company_type', inverse='_write_company_type', store=True)
 
     def create(self, vals_list): 
         res = super(ResPartner, self).create(vals_list)
