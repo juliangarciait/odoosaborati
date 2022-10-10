c2abd838e0e921869b1b93100e52ebb1f124f4ea
ArmandoBeltran <beltran.armando2210@gmail.com>
2022-10-10 14:08:18 -0500

    .

--- saborati/models/res_partner.py
+++ saborati/models/res_partner.py
@@ -5,14 +5,14 @@ from odoo import models, api, fields, _
 class ResPartner(models.Model): 
     _inherit = 'res.partner'
     
-    company_filter = fields.Selection(
-        selection=[('person', 'Individual'), ('company', 'Company')],
-        compute='_compute_company_filter', store=True)
+    #company_filter = fields.Selection(
+    #    selection=[('person', 'Individual'), ('company', 'Company')],
+    #    compute='_compute_company_filter', store=True)
     
-    @api.depends('company_type')
-    def _compute_company_filter(self): 
-        for partner in self: 
-            partner.company_filter = partner.company_type
+    #@api.depends('company_type')
+    #def _compute_company_filter(self): 
+    #    for partner in self: 
+    #        partner.company_filter = partner.company_type
 
     def create(self, vals_list): 
         res = super(ResPartner, self).create(vals_list)
