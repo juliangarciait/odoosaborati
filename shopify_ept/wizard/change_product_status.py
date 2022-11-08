6642b106f6073a23acb1ec9baf47e0d390c1d75d
ArmandoBeltran <beltran.armando2210@gmail.com>
2022-11-08 13:38:09 -0600

    Cambios

--- shopify_ept/wizard/change_product_status.py
+++ shopify_ept/wizard/change_product_status.py
@@ -14,15 +14,16 @@ class ChangeProductStatus(models.TransientModel):
     def apply(self): 
         product_ids = self.env['shopify.product.template.ept'].search([('id', 'in', self._context['ids'])])
 
-        for product in product_ids: 
-            product.product_status = self.product_status
-            export_data = self.env['shopify.process.import.export'].create({
-                'shopify_instance_id' : product.shopify_instance_id.id,
-                'shopify_is_set_basic_detail' : True,
-                'shopify_is_update_basic_detail' : True,
-                'shopify_is_set_price' : True,
-                'shopify_is_set_image' : True,
-                'shopify_is_publish' : 'publish_product_global',
-            })
-            export_data.with_context({"active_ids" : [product.id]}).manual_update_product_to_shopify()
+        for product in product_ids:
+            if product.exported_in_shopify: 
+                product.product_status = self.product_status
+                export_data = self.env['shopify.process.import.export'].create({
+                    'shopify_instance_id'            : product.shopify_instance_id.id,
+                    'shopify_is_set_basic_detail'    : True,
+                    'shopify_is_update_basic_detail' : True,
+                    'shopify_is_set_price'           : True,
+                    'shopify_is_set_image'           : True,
+                    'shopify_is_publish'             : 'publish_product_global',
+                })
+                export_data.with_context({"active_ids" : [product.id]}).manual_update_product_to_shopify()
 
