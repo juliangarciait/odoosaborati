1cf57a02e026016c930e870118296d2221856257
ArmandoBeltran <beltran.armando2210@gmail.com>
2022-11-30 17:20:16 -0600

    Se agregan cambios en la pestaña de precios y también ya se calcula el costo por variante

--- saborati/models/product_product.py
+++ saborati/models/product_product.py
@@ -2,6 +2,40 @@
 
 from odoo import models, api, _, fields
 
+import logging
+_logger = logging.getLogger(__name__)
 
 class ProductProduct(models.Model): 
     _inherit = 'product.product'
+    
+    list_price = fields.Float(compute="_compute_price")
+    
+    replacement_cost = fields.Float(compute="_compute_replacement_cost")
+    
+    @api.depends('seller_ids', 'bom_ids')
+    def _compute_replacement_cost(self): 
+        for record in self: 
+            record.replacement_cost = 0.0
+            has_mrp_bom = self.env['mrp.bom'].search([('product_id', '=', record.id)], order='write_date desc', limit=1)
+            if not has_mrp_bom: 
+                vendor_pricelist = self.env['product.supplierinfo'].search([('product_tmpl_id', '=', record.id)], order='create_date desc', limit=1)
+                if vendor_pricelist and vendor_pricelist.currency_id.id != self.env.company.currency_id.id: 
+                    price = vendor_pricelist.currency_id._convert(vendor_pricelist.price, self.env.company.currency_id, self.env.company, vendor_pricelist.create_date)
+                else: 
+                    price = vendor_pricelist.price
+                record.replacement_cost = price
+            elif has_mrp_bom: 
+                record.replacement_cost = has_mrp_bom.replacement_cost_total
+                
+            #costs = self.env['additional.cost'].search([('product_tmpl_id', '=', record.id)])
+            #if costs:
+            #    for cost in costs: 
+            #        record.replacement_cost += cost.cost
+            
+    @api.depends('margin_ids', 'replacement_cost')
+    def _compute_price(self): 
+        for record in self:
+            record.list_price = 1.0
+            margin = self.env['product.margin'].search([('product_tmpl_id', '=', record.product_tmpl_id.id)], order='create_date desc', limit=1).margin
+            if margin and record.replacement_cost:   
+                record.list_price = record.replacement_cost / (1 - margin)
--- saborati/models/product_template.py
+++ saborati/models/product_template.py
@@ -28,31 +28,33 @@ class ProductTemplate(models.Model):
 
     product_collection_ids = fields.Many2many('shopify.product.collection', string="Collections")
     
+    product_prices_ids = fields.One2many('product.prices', 'product_tmpl_id')
+    
     first_product_pricelist_id = fields.Many2one('product.pricelist', '1. Lista de Precios')
-    first_product_price = fields.Char('Precio', readonly="1", store=True)
+    first_product_price = fields.Char('Precio', readonly="1", store=True, compute='_compute_first_product_pricelist')
     
     second_product_pricelist_id = fields.Many2one('product.pricelist', '2. Lista de Precios')
-    second_product_price = fields.Char('Precio', readonly="1", store=True)
+    second_product_price = fields.Char('Precio', readonly="1", store=True, compute='_compute_second_product_pricelist')
     
     third_product_pricelist_id = fields.Many2one('product.pricelist', '3. Lista de Precios')
-    third_product_price = fields.Char('Precio', readonly="1", store=True)
+    third_product_price = fields.Char('Precio', readonly="1", store=True, compute='_compute_third_product_pricelist')
     
-    @api.onchange('first_product_pricelist_id')
-    def _onchange_first_product_pricelist(self): 
+    @api.depends('first_product_pricelist_id', 'first_product_pricelist_id.item_ids')
+    def _compute_first_product_pricelist(self): 
         for record in self: 
             if record.first_product_pricelist_id: 
                 price_with_tax = record.taxes_id.compute_all(float(record.first_product_pricelist_id.get_product_price(record.product_variant_id, 1.0, partner=False, uom_id=record.product_variant_id.uom_id.id)), product=record, partner=self.env['res.partner'])
                 record.first_product_price = str(record.first_product_pricelist_id.get_product_price(record.product_variant_id, 1.0, partner=False, uom_id=record.product_variant_id.uom_id.id)) + " (" + str(float(price_with_tax['total_included'])) + " con impuestos)"
         
-    @api.onchange('second_product_pricelist_id')
-    def _onchange_second_product_pricelist(self): 
+    @api.depends('second_product_pricelist_id', 'second_product_pricelist_id.item_ids')
+    def _compute_second_product_pricelist(self): 
         for record in self:
             if record.second_product_pricelist_id:  
                 price_with_tax = record.taxes_id.compute_all(float(record.second_product_pricelist_id.get_product_price(record.product_variant_id, 1.0, partner=False, uom_id=record.product_variant_id.uom_id.id)), product=record, partner=self.env['res.partner'])
                 record.second_product_price = str(record.second_product_pricelist_id.get_product_price(record.product_variant_id, 1.0, partner=False, uom_id=record.product_variant_id.uom_id.id)) + " (" + str(float(price_with_tax['total_included'])) + " con impuestos)"
         
-    @api.onchange('third_product_pricelist_id')
-    def _onchange_third_product_pricelist(self): 
+    @api.depends('third_product_pricelist_id', 'third_product_pricelist_id.item_ids')
+    def _compute_third_product_pricelist(self): 
         for record in self:    
             if record.third_product_pricelist_id:
                 price_with_tax = record.taxes_id.compute_all(float(record.third_product_pricelist_id.get_product_price(record.product_variant_id, 1.0, partner=False, uom_id=record.product_variant_id.uom_id.id)), product=record, partner=self.env['res.partner'])
@@ -71,8 +73,9 @@ class ProductTemplate(models.Model):
     @api.depends('seller_ids', 'bom_ids', 'additional_cost_ids')
     def _compute_replacement_cost(self): 
         for record in self: 
-            record.replacement_cost = 0.0
-            has_mrp_bom = self.env['mrp.bom'].search([('product_tmpl_id', '=', record.id)], order='write_date desc', limit=1)
+            has_mrp_bom = self.env['mrp.bom'].search([('product_tmpl_id', '=', record.id), ('product_id', '=', False)], order='write_date desc', limit=1)
+
+            
             if not has_mrp_bom: 
                 vendor_pricelist = self.env['product.supplierinfo'].search([('product_tmpl_id', '=', record.id)], order='create_date desc', limit=1)
                 if vendor_pricelist and vendor_pricelist.currency_id.id != self.env.company.currency_id.id: 
