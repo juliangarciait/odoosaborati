0b25a49d96deadbaa42d77ca3d83a544dec3977e
ArmandoBeltran <beltran.armando2210@gmail.com>
2022-12-27 17:07:14 -0600

    Se coloca que el costo adicional sea independiente de cada compañía

--- saborati/models/additional_cost.py
+++ saborati/models/additional_cost.py
@@ -5,7 +5,9 @@ from odoo import models, fields, api, _
 class AdditionalCost(models.Model): 
     _name = 'additional.cost'
     
-    cost = fields.Float(string='Costo adicional')
-    note = fields.Text(string='Notas')
+    company_id = fields.Many2one('res.company', string='Company',  default=lambda self: self.env.company.id, company_dependent=True)
+    
+    cost = fields.Float(string='Costo adicional', company_dependent=True)
+    note = fields.Text(string='Notas', company_dependent=True)
 
-    product_tmpl_id = fields.Many2one('product.template')
+    product_tmpl_id = fields.Many2one('product.template', company_dependent=True)
