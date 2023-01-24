80e337bb965aec0a4daea374dd12725dc8b40f27
JESUS <you@example.com>
2023-01-24 08:56:06 -0500

    oka

--- linio_connector_vex/models/vex_customer.py
+++
@@ -1,5 +0,0 @@
-from odoo import api, fields, models
-class Customers(models.Model):
-    _name           = 'res.partner'
-    _inherit        = 'res.partner'
-    conector = fields.Selection(selection_add=[('linio', 'Linio')])
