61533dd46ae1d9664e3058873053e39682ef646d
ArmandoBeltran <beltran.armando2210@gmail.com>
2023-01-27 19:38:58 -0600

    Cambios

--- saborati/models/product_supplierinfo.py
+++ saborati/models/product_supplierinfo.py
@@ -24,9 +24,9 @@ class ProductSupplierinfo(models.Model):
                 export_data = self.env['shopify.process.import.export'].create({
                     'shopify_instance_id' : product.shopify_instance_id.id,
                     'shopify_is_set_basic_detail' : True,
-                    'shopify_is_update_basic_detail' : False,
+                    'shopify_is_update_basic_detail' : True,
                     'shopify_is_set_price' : True,
-                    'shopify_is_set_image' : False,
+                    'shopify_is_set_image' : True,
                     'shopify_is_publish' : 'publish_product_global',
                 })
                 if product.exported_in_shopify:
