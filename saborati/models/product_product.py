0f049887d8600ae507b5cc0d5e30c44d6e45c6ca
ArmandoBeltran <beltran.armando2210@gmail.com>
2023-03-09 15:08:45 -0600

    costo de reposicion

--- saborati/models/product_product.py
+++ saborati/models/product_product.py
@@ -66,4 +66,4 @@ class ProductProduct(models.Model):
                 _logger.info(record.replacement_cost)
                 _logger.info(margin)
                 _logger.info('$Replacement$'*1000)
-                record.list_price = record.with_context(force_company=self.env.company.id).replacement_cost / (1 - margin)
+                record.list_price = record.replacement_cost / (1 - margin)
