4d6789c3f5a8f8315eb2c55ac696748cda3bb286
ArmandoBeltran <beltran.armando2210@gmail.com>
2023-05-11 15:07:59 -0600

    Cambios

--- saborati/models/product_product.py
+++ saborati/models/product_product.py
@@ -59,36 +59,13 @@ class ProductProduct(models.Model):
             record.replacement_cost = 0.0
             has_mrp_bom = self.env['mrp.bom'].search([('product_id', '=', record.id), ('company_id', '=', self.env.company.id)], order='write_date desc', limit=1)
 
-            #if record.product_tmpl_id.product_variant_id.id == record.id:
-            #    if not has_mrp_bom:
-            #        record.replacement_cost = record.product_tmpl_id.replacement_cost
-            #    else:
-            #        record.replacement_cost = has_mrp_bom.replacement_cost_total
-            #else: 
-            if not has_mrp_bom: 
-                price = 0.0
-                vendor_pricelist = self.env['product.supplierinfo'].search([('product_tmpl_id', '=', record.product_tmpl_id.id), ('company_id', '=', self.env.company.id)], order='create_date desc', limit=1)
-                po_price = self.env['purchase.order.line'].search([('state', '=', 'purchase'), ('product_id', '=', record.id), ('company_id', '=', self.env.company.id)], order='write_date desc', limit=1)
-    
-                if po_price and vendor_pricelist: 
-                    vendor_date = datetime.strptime(str(vendor_pricelist.write_date), '%Y-%m-%d %H:%M:%S.%f')
-                    po_date = datetime.strptime(str(po_price.write_date), '%Y-%m-%d %H:%M:%S.%f')
-                    if po_date > vendor_date: 
-                        price = po_price.price_unit 
-                    else: 
-                        if vendor_pricelist.currency_id.id != self.env.company.currency_id.id: 
-                            price = vendor_pricelist.currency_id._convert(vendor_pricelist.price, self.env.company.currency_id, self.env.company, vendor_pricelist.create_date)
-                        else: 
-                            price = vendor_pricelist.price
-                elif vendor_pricelist and not po_price:
-                    if vendor_pricelist.currency_id.id != self.env.company.currency_id.id: 
-                        price = vendor_pricelist.currency_id._convert(vendor_pricelist.price, self.env.company.currency_id, self.env.company, vendor_pricelist.create_date)
-                    else: 
-                        price = vendor_pricelist.price
-                elif not vendor_pricelist and po_price:
-                    price = po_price.price_unit 
-                    
-                record.replacement_cost = price
+            if record.product_tmpl_id.product_variant_id.id == record.id:
+                if not has_mrp_bom:
+                    record.replacement_cost = self.calculate_if_not_mrp_bom(record)
+                else:
+                    record.replacement_cost = has_mrp_bom.replacement_cost_total 
+            elif not has_mrp_bom: 
+                record.replacement_cost = self.calculate_if_not_mrp_bom(record)
             elif has_mrp_bom: 
                 record.replacement_cost = has_mrp_bom.replacement_cost_total
                 
@@ -96,6 +73,31 @@ class ProductProduct(models.Model):
             #if costs:
             #    for cost in costs: 
             #        record.replacement_cost += cost.cost
+            
+    def calculate_if_not_mrp_bom(self, product):
+        cost = 0.0
+        vendor_pricelist = self.env['product.supplierinfo'].search([('product_tmpl_id', '=', product.product_tmpl_id.id), ('company_id', '=', self.env.company.id)], order='create_date desc', limit=1)
+        po_price = self.env['purchase.order.line'].search([('state', '=', 'purchase'), ('product_id', '=', product.id), ('company_id', '=', self.env.company.id)], order='write_date desc', limit=1)
+
+        if po_price and vendor_pricelist: 
+            vendor_date = datetime.strptime(str(vendor_pricelist.write_date), '%Y-%m-%d %H:%M:%S.%f')
+            po_date = datetime.strptime(str(po_price.write_date), '%Y-%m-%d %H:%M:%S.%f')
+            if po_date > vendor_date: 
+                cost = po_price.price_unit 
+            else: 
+                if vendor_pricelist.currency_id.id != self.env.company.currency_id.id: 
+                    cost = vendor_pricelist.currency_id._convert(vendor_pricelist.price, self.env.company.currency_id, self.env.company, vendor_pricelist.create_date)
+                else: 
+                    cost = vendor_pricelist.price
+        elif vendor_pricelist and not po_price:
+            if vendor_pricelist.currency_id.id != self.env.company.currency_id.id: 
+                cost = vendor_pricelist.currency_id._convert(vendor_pricelist.price, self.env.company.currency_id, self.env.company, vendor_pricelist.create_date)
+            else: 
+                cost = vendor_pricelist.price
+        elif not vendor_pricelist and po_price:
+            cost = po_price.price_unit
+            
+        return cost
             
     @api.depends('margin_ids', 'replacement_cost')
     def _compute_price(self): 
