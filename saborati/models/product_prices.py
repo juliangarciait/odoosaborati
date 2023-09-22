e9d72e9b0d26a44a39fbaa03a6d90d8369616e68
Luis Ernesto Garcia Medina <ernesto.r.2.em@gmail.com>
2023-09-21 19:10:28 -0600

    Mejoras para calculos de precios en productos archivados

--- saborati/models/product_prices.py
+++ saborati/models/product_prices.py
@@ -15,11 +15,15 @@ class ProductPrices(models.Model):
     
     @api.depends('product_pricelist_id', 'product_pricelist_id.item_ids')
     def _compute_product_pricelist(self): 
-        for record in self: 
+        for record in self:
+            price = 0
+            price_with_tax_amount = 0
             if record.product_pricelist_id:
-                price = record.product_pricelist_id.get_product_price(record.product_tmpl_id.product_variant_id, 1.0, partner=False, uom_id=record.product_tmpl_id.product_variant_id.uom_id.id)
-                price_with_tax = record.product_tmpl_id.taxes_id.compute_all(float(price), product=record.product_tmpl_id, partner=self.env['res.partner']) 
-                record.price = str(float(price)) + " (" + str(float(price_with_tax['total_included'])) + " con impuestos)"   
+                if record.product_tmpl_id.product_variant_id:
+                    price = record.product_pricelist_id.get_product_price(record.product_tmpl_id.product_variant_id, 1.0, partner=False, uom_id=record.product_tmpl_id.product_variant_id.uom_id.id)
+                    price_with_tax = record.product_tmpl_id.taxes_id.compute_all(float(price), product=record.product_tmpl_id, partner=self.env['res.partner']) 
+                    price_with_tax_amount = price_with_tax['total_included']
+            record.price = str(float(price)) + " (" + str(float(price_with_tax_amount)) + " con impuestos)"   
                 
                 
 class ProductProductPrices(models.Model): 
